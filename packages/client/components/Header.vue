5e34594a2dc2e1b6d6de7d50f06a7d71580d0229
thieuflower <thieuflower01@gmail.com>
2023-07-29 17:37:36 +0700

    pending task login modal

--- packages/client/components/Header.vue
+++ packages/client/components/Header.vue
@@ -1,48 +1,96 @@
 <template>
   <div>
     <header class="flex justify-between m-4">
-      <div class="font-bold text-sm text-center uppercase text-red-500">
+      <div
+        class="font-bold text-sm text-center uppercase text-blue-500 flex items-center"
+      >
+        <!-- <img src="/images/logo.png" alt="Logo" class="logo mr-2" /> -->
         <nuxt-link to="https://hoppscotch.io/" target="_blank">
-          Hoppscotch
+          LOTUS
         </nuxt-link>
       </div>
-      <div>
+      <div class="flex items-center">
         <Icon name="uil:search" class="w-6 h-6" />
-        <el-button type="primary" class="ml-2" @click="showLoginModal">
+        <el-button type="primary" class="ml-2 btn-x1" @click="showLoginModal">
           Login
         </el-button>
-        <el-button type="success">Success</el-button>
       </div>
     </header>
     <ClientOnly>
       <el-dialog
-        title="Login to Lotus"
         v-model="visibleModalLogin"
-        width="30%"
+        width="400px"
         append-to-body
+        title="Login to Lotus"
+        class="rounded-lg dialog-modal"
+        data-v-5a4a5a9a
       >
-        <div class="mr-5">
-          <el-row>
-            <Icon name="uil:github" class="w-6 h-6" />
-            <span>Continue with Github</span>
-          </el-row>
-          <el-row>
-            <Icon name="uil:google" class="w-6 h-6" />
-            <span>Continue with Google</span>
-          </el-row>
-          <el-row>
-            <Icon name="uil:mail" class="w-6 h-6" />
-            <span>Continue with Email</span>
-          </el-row>
-        </div>
+        <el-divider />
+        <el-row :gutter="16">
+          <el-col :span="2">
+            <Icon name="akar-icons:github-fill" class="w-6 h-6" />
+          </el-col>
+          <el-col :span="22">
+            <span class="ml-3 icon-text">Continue with Github</span>
+          </el-col>
+        </el-row>
+        <el-row class="mt-2">
+          <el-col :span="2">
+            <Icon name="logos:google-gmail" class="w-6 h-6" />
+          </el-col>
+          <el-col :span="22">
+            <span class="ml-3 icon-text">Continue with Google</span>
+          </el-col>
+        </el-row>
+        <el-row class="mt-2">
+          <el-col :span="2">
+            <Icon name="material-symbols:mail" class="w-6 h-6" />
+          </el-col>
+          <el-col :span="22">
+            <span class="ml-3 icon-text" @click="showEmailModal"
+              >Continue with Email</span
+            >
+          </el-col>
+        </el-row>
+
+        <!-- </div> -->
+        <!--<hr class="w-full border-t border-gray-300 my-4"> 
         <span slot="footer" class="dialog-footer">
           <el-button @click="hiddenLoginModal">Cancel</el-button>
           <el-button type="primary">Login</el-button>
-        </span>
+        </span> -->
+      </el-dialog>
+      <el-dialog
+        v-model="visibleEmailModal"
+        title="Login to Lotus"
+        width="400px"
+        class="rounded-lg"
+      >
+        <div>
+          <el-form class="items-center mb-4">
+            <el-form-item label="Email" :label-width="formLabelWidth">
+              <el-input
+                v-model="email"
+                placeholder="Enter your email"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="Password" :label-width="formLabelWidth">
+              <el-input
+                v-model="password"
+                placeholder="Enter your password"
+                type="password"
+              ></el-input>
+            </el-form-item>
+          </el-form>
+          <hr class="w-full border-t border-gray-300 my-4" />
+          <span slot="footer" class="dialog-footer flex justify-center">
+            <!-- <el-button @click="visibleEmailModal = false">Cancel</el-button> -->
+            <el-button type="primary">Login</el-button>
+          </span>
+        </div>
       </el-dialog>
     </ClientOnly>
   </div>
-
   <!-- </ClientOnly> -->
 </template>
 
@@ -50,14 +98,34 @@
 import { ref } from 'vue';
 
 const visibleModalLogin = ref(false);
-
 const showLoginModal = () => {
   visibleModalLogin.value = true;
 };
-
 const hiddenLoginModal = () => {
   visibleModalLogin.value = false;
 };
+
+const visibleEmailModal = ref(false);
+const showEmailModal = () => {
+  visibleEmailModal.value = true;
+};
+const hiddenEmailModal = () => {
+  visibleEmailModal.value = false;
+};
+const formLabelWidth = '70px';
 </script>
 
-<style scoped></style>
+<style module>
+.dialog-modal .el-dialog__body {
+  padding: 0px 20px 20px 20px !important;
+}
+
+.dialog-modal .el-dialog__title {
+  font-size: x-large;
+}
+
+.icon {
+  width: 24px;
+  height: 24px;
+}
+</style>
